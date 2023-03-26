2f324a388bf86779a22e77ce3144f19489e37f8c
Dmitry <d.myachn@gmail.com>
2023-03-26 03:12:48 +0300

    done

--- lastOne/ViewController.swift
+++ lastOne/ViewController.swift
@@ -7,13 +7,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        view.backgroundColor = .systemCyan
+        title = "Collection"
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        let layout = CustomFlow()
+        layout.scrollDirection = .horizontal
+        self.collectionView.collectionViewLayout = layout
+        self.collectionView!.contentInset = UIEdgeInsets(top: 40, left: 30, bottom:0, right: 0)
 
+        if let layout = self.collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.minimumInteritemSpacing = 10
+            layout.minimumLineSpacing = 10
+            layout.itemSize = CGSize(width: 280, height: 400)
+            layout.invalidateLayout()
+        }
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+      }
+
+      override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+      }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .black
+        cell.backgroundColor = .systemGray
+        cell.layer.cornerCurve = .continuous
+        cell.layer.cornerRadius = 25
+        return cell
+    }
 
 }
 
+class CustomFlow: UICollectionViewFlowLayout{
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+
+        guard let collectionView = self.collectionView else {
+            let latestOffset = super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+            return latestOffset
+        }
+
+        let pageWidth = self.itemSize.width + self.minimumInteritemSpacing
+        let approximatePage = collectionView.contentOffset.x/pageWidth
+        let currentPage = velocity.x == 0 ? round(approximatePage) : (velocity.x < 0.0 ? floor(approximatePage) : ceil(approximatePage))
+        let flickVelocity = velocity.x * 0.3
+        let flickedPages = (abs(round(flickVelocity)) <= 1) ? 0 : round(flickVelocity)
+        let newHorizontalOffset = ((currentPage + flickedPages) * pageWidth) - collectionView.contentInset.left
+
+        return CGPoint(x: newHorizontalOffset, y: proposedContentOffset.y)
+    }
+}
